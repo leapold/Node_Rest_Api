e830897657b4316287b5476cd795ee7f4eaf7178
leapold <koyler_leonid@hotmail.com>
2024-09-18 21:22:45 -0400

    Add  Cross-origin resource sharing (CORS) and parsers

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+//npm install --save-dev nodemon
 const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
 
+const bodyParser = require("body-parser");
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 //all request pass morgan
+//npm install --save morgan
 app.use(morgan('dev'));
+
+//npm install --save body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cross-origin resource sharing (CORS) is a mechanism for integrating applications.
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");// access to any client
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");// what type of header allows
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
