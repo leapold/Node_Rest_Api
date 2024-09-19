39ef0f541f6f336ca95b0dc4954908bd54308965
leapold <koyler_leonid@hotmail.com>
2024-09-18 20:17:01 -0400

    Add coments

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+// ./api/routes/products/get
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
 });
 
+// ./api/routes/products/post
 router.post('/', (req, res, next) => {
     res.status(201).json({
         message: 'Handling POST requests to /products'
     });
 });
 
+// ./api/routes/products/get/2
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     if (id === 'special') {
@@ -27,6 +30,7 @@ router.get('/:productId', (req, res, next) => {
     }
 });
 
+// ./api/routes/products/patch/3
 router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Updated product!'
